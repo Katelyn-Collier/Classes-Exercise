3283f8747514b6223adc41bfc707812c8bfc90d2
Katelyn-Collier <katelyncollier14@gmail.com>
2023-01-25 19:30:15 -0600

    initial commit

---
+++ Classes/Car.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    public class Car
+    {
+        public Car()
+        { 
+        
+        }
+        public Car(string make, string model, string color, int year) 
+        { 
+            Make = make;
+            Model = model;
+            Color = color;
+            Year = year;
+        }
+
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public string Color { get; set; }
+        public int Year { get; set; }
+    }
+}
--- Classes/Program.cs
+++ Classes/Program.cs
@@ -4,7 +4,29 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Car myCar = new Car();
+            myCar.Make = "Toyota";
+            myCar.Model = "Corolla";
+            myCar.Color = "Silver";
+            myCar.Year = 2021;
+
+            var secondCar = new Car()
+            {
+                Make = "Ford",
+                Model = "Mustang",
+                Color = "White",
+                Year = 1997
+            };
+
+            var thirdCar = new Car("Jeep", "Wrangler", "Black", 2017);
+
+            var carList = new List<Car>() { myCar, secondCar, thirdCar};
+
+            foreach (var vehicle in carList)
+            {
+                Console.WriteLine($"{vehicle.Make} {vehicle.Model} {vehicle.Color} {vehicle.Year}");
+            }
+
         }
     }
 }
